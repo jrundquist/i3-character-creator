05b8861134027c8f8ef722966b35c2f504084c79
James Rundquist <jrscienceguy@gmail.com>
2011-11-15 20:26:17 -0500

    Better fail message when loading card image

--- ajax/cards.php
+++ ajax/cards.php
@@ -6,20 +6,24 @@ require_once('../models/card.php');
 
 $card = isset($_REQUEST['id'])?new Card($_REQUEST['id']):false;
 
-if ( $card ){
-	$toret = array(
-		"frontimg" => $card->getStat("picture"),
-		"backimg"  => $card->getStat("backpic"),
-		"listname" => createCardListName($card),
-		"cardtype" => $card->getTypeAsStr()
-	);
-
-	echo '
-	<h1>'.preg_replace('%\[\d+\]%', '', $toret['listname']).'</h1>
-	<div class="card-container">
-		<img id="front-card" class="bigImg" src="http://testcb.untoldthegame.com/Version1.3/card_imgs/'.$toret['backimg'].'"/>
-	</div>
-	<div class="card-container">
-		<img id="back-card"  class="bigImg" src="http://testcb.untoldthegame.com/Version1.3/card_imgs/'.$toret['frontimg'].'"/>
-	</div>';
+if ( !$card ){
+	echo '<h1>Unable to find specified card.</h1>';
+	die();
 }
+
+
+$toret = array(
+	"frontimg" => $card->getStat("picture"),
+	"backimg"  => $card->getStat("backpic"),
+	"listname" => createCardListName($card),
+	"cardtype" => $card->getTypeAsStr()
+);
+
+echo '
+<h1>'.preg_replace('%\[\d+\]%', '', $toret['listname']).'</h1>
+<div class="card-container">
+	<img id="front-card" class="bigImg" src="http://testcb.untoldthegame.com/Version1.3/card_imgs/'.$toret['backimg'].'"/>
+</div>
+<div class="card-container">
+	<img id="back-card"  class="bigImg" src="http://testcb.untoldthegame.com/Version1.3/card_imgs/'.$toret['frontimg'].'"/>
+</div>';
